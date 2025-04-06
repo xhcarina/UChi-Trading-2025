a7eb8e4c07f1a8f5331907785b4f6579d1aa828c
Carina <apple@Carinas-MacBook-Pro.local>
2025-04-06 00:34:51 -0500

    new

--- case2/MainBot.py
+++ case2/MainBot.py
@@ -13,6 +13,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.decomposition import PCA
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, precision_score, recall_score, f1_score
+from sklearn.model_selection import train_test_split
 
 
 
